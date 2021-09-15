6995ac4804af4624852d59e3b14bb211a862b42f
Jihan Alfi <jihanalfi67@gmail.com>
2021-09-15 19:41:33 +0700

    feat: tutorial 2 - kebun safari project

---
+++ kebunsafari/src/main/java/apap/tutorial/kebunsafari/service/KebunSafariInMemoryService.java
@@ -0,0 +1,44 @@
+package apap.tutorial.kebunsafari.service;
+
+import apap.tutorial.kebunsafari.model.KebunSafariModel;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.ArrayList;
+
+@Service
+public class KebunSafariInMemoryService implements KebunSafariService{
+    private List<KebunSafariModel> listKebunSafari;
+
+    // Constructor
+    public KebunSafariInMemoryService() {
+        listKebunSafari = new ArrayList<>();
+    }
+
+    @Override
+    public void addKebunSafari(KebunSafariModel kebunSafari){
+        listKebunSafari.add(kebunSafari);
+    }
+
+    @Override
+    public void deleteKebunSafari(KebunSafariModel kebunSafari){
+        if (listKebunSafari.contains(kebunSafari)) {
+            listKebunSafari.remove(kebunSafari);
+        }
+    }
+
+    @Override
+    public List<KebunSafariModel> getKebunSafariList(){
+        return listKebunSafari;
+    }
+
+    @Override
+    public KebunSafariModel getKebunSafariByIdKebunSafari(String idKebunSafari){
+        for (KebunSafariModel kebunSafari : listKebunSafari){
+            if (kebunSafari.getIdKebunSafari().equals(idKebunSafari)) {
+                // System.out.print("masuk if");
+                return kebunSafari;
+            }
+        }
+        return null;
+    }
+}
